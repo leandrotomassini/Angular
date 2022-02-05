5eb5066ea02a4e28138c55edc8baf0ca1aab8318
unknown <leandroctomassini@gmail.com>
2022-02-05 16:43:41 -0300

    Angular I

---
+++ workspace/01-Bases/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/herore.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContadorComponent,
+    HeroeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ workspace/01-Bases/src/app/heroes/heroe/herore.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: 'heroe.component.html'
+})
+export class HeroeComponent {
+    nombre: string = 'Iron man';
+    edad: number = 45;
+
+    get nombreCapitalizado(): string {
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre(): string {
+        return `${this.nombre} - ${this.edad}`;
+    }
+
+    cambiarNombre(): void {
+        this.nombre = 'Spiderman';
+    }
+}
