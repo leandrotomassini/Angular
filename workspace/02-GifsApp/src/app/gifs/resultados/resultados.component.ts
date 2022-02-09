b44e9900d0b9532b18b1e6a289c4b7cdb70965c0
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-02-09 16:26:06 -0300

    Mostrar los resultados en pantalla

--- workspace/02-GifsApp/src/app/gifs/resultados/resultados.component.ts
+++ workspace/02-GifsApp/src/app/gifs/resultados/resultados.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-resultados',
@@ -6,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class ResultadosComponent implements OnInit {
+export class ResultadosComponent {
 
-  constructor() { }
+  constructor(private gifsService: GifsService) { }
 
-  ngOnInit(): void {
+  get resultados(): any {
+    return this.gifsService.resultados;
   }
 
 }
--- workspace/02-GifsApp/src/app/gifs/services/gifs.service.ts
+++ workspace/02-GifsApp/src/app/gifs/services/gifs.service.ts
@@ -9,6 +9,9 @@ export class GifsService {
   private apiKey: string = "fwHVLO449TE0bty2sFoKDrDRETR2g4F3";
   private _historial: string[] = [];
 
+  // TODO: cambiar any por su tipo correspondiente
+  public resultados: any[] = [];
+
   constructor(private http: HttpClient) { }
 
   buscarGifs(query: string): void {
@@ -20,10 +23,11 @@ export class GifsService {
       this._historial = this._historial.splice(0, 10);
     }
 
-    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=fwHVLO449TE0bty2sFoKDrDRETR2g4F3&q=dragon ball z&limit=10&lang=es')
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=fwHVLO449TE0bty2sFoKDrDRETR2g4F3&limit=10&q=${query}&lang=es`)
       .subscribe(
         (resp: any) => {
           console.log(resp.data);
+          this.resultados = resp.data;
         }
       );
 
