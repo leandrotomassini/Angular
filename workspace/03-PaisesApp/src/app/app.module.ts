af74df9b50de0466341520a7cb7efa49b3d9668f
unknown <leandroctomassini@gmail.com>
2022-02-17 18:53:55 -0300

    Paises rutas

---
+++ workspace/03-PaisesApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { PaisModule } from './pais/pais.module';
+
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PaisModule,
+    SharedModule,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
