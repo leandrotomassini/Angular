f64123a522752bd38d68e5fe26587517bafa1598
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-02-21 16:59:03 -0300

    LazyLoad

---
+++ workspace/05-heroesApp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ErrorPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
