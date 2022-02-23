d3011c13a7666fcb4e087ed91913194139807fcd
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-02-23 15:53:16 -0300

    Tienda

---
+++ workspace/5-Tienda/src/app/recap.ts
@@ -0,0 +1,16 @@
+
+const username: string = 'leandro';
+
+const suma = (a: number, b: number) => {
+  return a + b;
+}
+
+suma(1, 3);
+
+class Person {
+
+  constructor(public age: number, public lastName: string) {}
+
+}
+
+const leandro = new Person(14, 'rcok');
