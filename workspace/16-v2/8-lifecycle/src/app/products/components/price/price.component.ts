cf6e114ecf98de2602d0ad98b1e994c016f37b0d
Leandro C. Tomassini <leandroctomassini@gmail.com>
2024-05-31 18:24:28 -0300

    Sección 20

--- workspace/16-v2/8-lifecycle/src/app/products/components/price/price.component.ts
+++ workspace/16-v2/8-lifecycle/src/app/products/components/price/price.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
-@Component({
+@Component( {
   selector: 'products-price',
   templateUrl: './price.component.html',
   styles: ``
-})
-export class PriceComponent {
+} )
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+
+  @Input()
+  public price: number = 0;
+
+  public interval$?: Subscription;
+
+  ngOnInit(): void {
+    console.log( 'Componente hijo: ngOnInit' );
+    this.interval$ = interval( 1000 ).subscribe( value => console.log( `Tick ${ value }` ) );
+
+  }
+
+  ngOnChanges( changes: SimpleChanges ): void {
+    console.log( 'Componente hijo: ngOnChanges' );
+    console.log( { hijo: changes } );
+  }
+
+  ngOnDestroy(): void {
+    console.log( 'Componente hijo: ngOnDestroy' );
+    this.interval$?.unsubscribe();
+  }
+
+
 
 }
--- workspace/16-v2/8-lifecycle/src/app/products/pages/product/product.component.ts
+++ workspace/16-v2/8-lifecycle/src/app/products/pages/product/product.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges
+} from '@angular/core';
 
-@Component({
+
+
+@Component( {
   selector: 'products-product-page',
   templateUrl: './product.component.html',
   styles: ``
-})
-export class ProductComponent {
+} )
+export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, OnDestroy, AfterViewChecked {
+
+  public isProductVisible: boolean = false;
+  public currentPrice: number = 10;
+
+  constructor() {
+    console.log( 'constructor' );
+  }
+
+  ngAfterViewChecked(): void {
+    console.log( 'ngAfterViewChecked' );
+  }
+
+  ngOnChanges( changes: SimpleChanges ): void {
+    console.log( { changes } );
+    console.log( 'ngOnChanges' );
+  }
+
+  ngDoCheck(): void {
+    console.log( 'ngDoCheck' );
+  }
+
+  ngAfterContentInit(): void {
+    console.log( 'ngAfterContentInit' );
+  }
+
+  ngAfterContentChecked(): void {
+    console.log( 'ngAfterContentChecked' );
+  }
+
+  ngAfterViewInit(): void {
+    console.log( 'ngAfterViewInit' );
+  }
+
+  ngOnDestroy(): void {
+    console.log( 'ngOnDestroy' );
+  }
+
+  ngOnInit(): void {
+    console.log( 'ngOnInit' );
+  }
+
+  increasePrice() {
+    this.currentPrice++;
+  }
 
 }
